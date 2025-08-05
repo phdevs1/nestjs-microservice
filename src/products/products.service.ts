f7071f5e37a8f22224157969959f4e3cf3e1a012
ph <pio.huisa@gmail.com>
2025-08-05 11:59:25 -0500

    feat: add uuid and CRUD products

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,26 +1,42 @@
 import { Injectable } from '@nestjs/common';
+import { v4 as UuidV4 } from 'uuid';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { Product } from './entities/product.entity';
 
 @Injectable()
 export class ProductsService {
+  private products: Product[] = [];
+
   create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+    const { name, description = '', price = 0 } = createProductDto;
+    const newProduct = new Product(UuidV4(), name, description, price);
+    this.products.push(newProduct);
+    return newProduct;
   }
 
   findAll() {
-    return `This action returns all products`;
+    return this.products;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  findOne(id: string) {
+    const product = this.products.find((p) => p.id === id);
+    if (!product) {
+      throw new Error(`Product with id ${id} not found`);
+    }
+    return product;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  update(id: string, updateProductDto: UpdateProductDto): Product {
+    const { name, description, price } = updateProductDto;
+    const product = this.findOne(id);
+    product.updateWith({ name, description, price });
+    return product;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  remove(id: string): Product {
+    const product = this.findOne(id);
+    this.products = this.products.filter((p) => p.id !== id);
+    return product;
   }
 }
