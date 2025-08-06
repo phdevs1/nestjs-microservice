26eb894341a677476a63faef50897b8528ea9a54
ph <pio.huisa@gmail.com>
2025-08-05 21:10:01 -0500

    feat: add env

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { envs } from './config/envs';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,6 +15,7 @@ async function bootstrap() {
     }),
   );
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(envs.PORT);
+  console.log(`Application is running on: http://localhost:${envs.PORT}/api`);
 }
 bootstrap();
